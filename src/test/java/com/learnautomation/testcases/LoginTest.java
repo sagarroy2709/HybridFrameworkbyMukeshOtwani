8ee1a8d5171a4782f19b0fa702383c11699b5188
divudilu <divudilu@gmail.com>
2020-04-24 17:19:30 +0530

    Update LoginTest.java

--- src/test/java/com/learnautomation/testcases/LoginTest.java
+++ src/test/java/com/learnautomation/testcases/LoginTest.java
@@ -34,7 +34,7 @@ public class LoginTest extends BaseClass {
 		String Adminurl=driver.getTitle();
 		if(Adminurl.equalsIgnoreCase("Primus Bank"))
 		{
-			Assert.assertTrue(false);
+			Assert.assertTrue(true);
 			
 		}else
 		{
@@ -50,7 +50,7 @@ public class LoginTest extends BaseClass {
 		
 		logger=reports.createTest("THe Primus Bank Application started");
 		logger.info("Logout to PrimusBank Application");
-		Assert.assertTrue(false);
+		Assert.assertTrue(true);
 		
 		
 		
